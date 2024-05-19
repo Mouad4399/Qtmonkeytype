d9a52e82a7d8d72d8c97e33b2431ec2957598144
Mouad4399 <mouad.aitougrram@usms.ac.ma>
2024-05-19 08:26:18 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtQml import QQmlApplicationEngine
+
+
+
+app = QApplication(sys.argv)
+engine= QQmlApplicationEngine()
+
+
+engine.load('main.qml')
+
+sys.exit(app.exec())
